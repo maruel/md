6334bfd4c0024e8276464bb4a5205d0f8d82f9b7
Marc-Antoine Ruel <maruel@gmail.com>
2026-02-11 14:57:08 -0500

    Move kimi-cli installation from 7_llm_tools.sh to 6_python.sh
    The installation of `kimi-cli` via `uv` was moved from `7_llm_tools.sh`
    to `6_python.sh`.
    
    The scripts are run in parallel for performance. Thus `uv` might not be
    initialized (available in PATH) when setup scripts run in parallel,
    causing the installation to fail in the LLM tools script.

--- rsc/home/user/setup/6_python.sh
+++ rsc/home/user/setup/6_python.sh
@@ -13,3 +13,6 @@ curl -LsSf https://astral.sh/uv/install.sh | sh -s -- --quiet
 uv tool install --quiet black
 uv tool install --quiet pylint
 uv tool install --quiet ruff
+# Kimi CLI: belongs in 7_llm_tools.sh but uv isn't guaranteed available there
+# due to parallel execution of setup scripts.
+uv tool install --quiet kimi-cli
